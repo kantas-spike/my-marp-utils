40f0971d7a950377a0a252489573849ccf276850
kanta <101532773+kantas-spike@users.noreply.github.com>
2025-09-26 22:41:26 +0900

    ツールを追加

---
+++ bin/extract_clip_text.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+"use strict";
+import { Marpit } from "@marp-team/marpit";
+import fs from "node:fs";
+import path from "node:path";
+import { program } from "commander";
+import { createRequire } from "node:module";
+
+function getClipInfo(marpFilePath) {
+  if (!fs.existsSync(marpFilePath)) {
+    throw new Error(`Marpファイル(${marpFilePath})が存在しません。`);
+  } else {
+    const marpit = new Marpit();
+    const data = fs.readFileSync(marpFilePath, "utf-8");
+    const { html, _css, comments } = marpit.render(data, {
+      htmlAsArray: true,
+    });
+    const clipInfoList = html
+      .map((elm, idx) => {
+        return {
+          hasClip: elm.match(/class=["']wm["']/) !== null,
+          slideNo: idx + 1,
+          html: elm,
+          cmment: comments[idx].map((elm) => elm.trimEnd()),
+        };
+      })
+      .filter((elm) => {
+        return elm.hasClip;
+      });
+    return clipInfoList;
+  }
+}
+
+const program_name = "marp2cliptext";
+const require = createRequire(import.meta.url);
+const { version } = require("../package.json");
+
+program
+  .name(program_name)
+  .version(version)
+  .description(
+    "Marpファイルからスライドに挿入するクリップ用のコメントを抽出し、テキストファイルに変換する"
+  )
+  .option(
+    "-o, --output <output_dir>",
+    "テキストファイルを出力するディレクトリパス"
+  )
+  .argument("<marp_file_path>", "原稿となるMarpファイルパス")
+  .action((marp_file_path, options) => {
+    try {
+      const infoList = getClipInfo(path.resolve(marp_file_path));
+      // console.log(infoList);
+
+      // 出力ディレクトリが未指定時
+      if (!options.output) {
+        console.warn(`出力先ディレクトリを指定してください。`);
+        process.exit(1);
+      }
+
+      const outputDir = path.resolve(options.output);
+      if (fs.existsSync(outputDir)) {
+        // 同名のファイルが存在する場合、エラー
+        if (!fs.lstatSync(outputDir).isDirectory()) {
+          console.warn(
+            `指定された出力先ディレクトリと同名のファイル(${outputDir})が既に存在します。別の出力先ディレクトリを指定してください。`
+          );
+          process.exit(1);
+        }
+      } else {
+        // 出力ディレクトリが存在しない場合、作成する
+        fs.mkdirSync(outputDir, { recursive: true });
+      }
+      console.log(`${outputDir}にクリップ用コメントを出力...`);
+      for (const info of infoList) {
+        const fileName = info.slideNo.toString().padStart(2, "0") + ".txt";
+        const outputFile = path.join(outputDir, fileName);
+        fs.writeFileSync(outputFile, info.cmment.join("\n"));
+        console.log(`  ${fileName}`);
+      }
+    } catch (err) {
+      console.error(err);
+      process.exit(-1);
+    }
+  });
+
+program.exitOverride((err) => {
+  let args = program.args || [];
+  if (!(args.includes("-h") || args.includes("--help"))) {
+    console.error(""); // 空行
+    program.outputHelp();
+    process.exit(err.exitCode);
+  }
+});
+
+program.parse();
---
+++ bin/extract_titles.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+"use strict";
+import { Marpit } from "@marp-team/marpit";
+import fs from "node:fs";
+import path from "node:path";
+import { program } from "commander";
+import { createRequire } from "node:module";
+import { JSDOM } from "jsdom";
+
+function getTitleInfo(marpFilePath) {
+  if (!fs.existsSync(marpFilePath)) {
+    throw new Error(`Marpファイル(${marpFilePath})が存在しません。`);
+  } else {
+    const marpit = new Marpit();
+    const data = fs.readFileSync(marpFilePath, "utf-8");
+    const { html, _css, _comments } = marpit.render(data, {
+      htmlAsArray: true,
+    });
+    // const titleInfo = {};
+    const titleInfo = html
+      .map((elm, idx) => {
+        // elmをパースしdom取得
+        const dom = JSDOM.fragment(elm);
+        // domから最初のH1 or H2を取得
+        let title = "no title";
+        const titleElm = dom.querySelector("h1,h2");
+        if (titleElm) {
+          title = titleElm.textContent;
+        }
+        return {
+          title,
+          slideNo: idx + 1,
+        };
+      })
+      .reduce((info, elm) => {
+        info[elm.slideNo] = elm.title;
+        return info;
+      }, {});
+    //console.log(titleInfo);
+    return titleInfo;
+  }
+}
+
+const program_name = "marp2titles";
+const require = createRequire(import.meta.url);
+const { version } = require("../package.json");
+
+program
+  .name(program_name)
+  .version(version)
+  .description("Marpファイルからスライドのタイトル一覧を取得する")
+  .option(
+    "-o, --output <output_file>",
+    "テキストファイルを出力するファイルパス"
+  )
+  .argument("<marp_file_path>", "原稿となるMarpファイルパス")
+  .action((marp_file_path, options) => {
+    try {
+      const info = getTitleInfo(path.resolve(marp_file_path));
+      // console.log(JSON.stringify(info, null, "    "));
+
+      // 出力ディレクトリが未指定時
+      if (!options.output) {
+        console.warn(`出力先のファイルパスを指定してください。`);
+        process.exit(1);
+      }
+
+      const outputPath = path.resolve(options.output);
+      if (fs.existsSync(outputPath)) {
+        // 同名のファイルが存在する場合、エラー
+        if (!fs.lstatSync(outputPath).isFile()) {
+          console.warn(
+            `指定された出力ファイルと同名のファイル(${outputPath})が既に存在します。`
+          );
+          process.exit(1);
+        }
+      } else {
+        fs.writeFileSync(outputPath, JSON.stringify(info, null, "    "));
+      }
+    } catch (err) {
+      console.error(err);
+      process.exit(-1);
+    }
+  });
+
+program.exitOverride((err) => {
+  let args = program.args || [];
+  if (!(args.includes("-h") || args.includes("--help"))) {
+    console.error(""); // 空行
+    program.outputHelp();
+    process.exit(err.exitCode);
+  }
+});
+
+program.parse();
